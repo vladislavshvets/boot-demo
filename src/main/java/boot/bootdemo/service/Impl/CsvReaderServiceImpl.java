7d63623207167b6291cf163a5e485eb460749dbe
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-11-16 20:56:29 +0200

    fixed check style

--- src/main/java/boot/bootdemo/service/Impl/CsvReaderServiceImpl.java
+++ src/main/java/boot/bootdemo/service/Impl/CsvReaderServiceImpl.java
@@ -1,8 +1,10 @@
 package boot.bootdemo.service.Impl;
 
+import boot.bootdemo.exception.FileReadException;
 import boot.bootdemo.service.CsvReaderService;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
@@ -13,8 +15,10 @@ public class CsvReaderServiceImpl implements CsvReaderService {
     public List<String> read(String filePath) {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             return reader.lines().collect(Collectors.toList());
+        } catch (FileNotFoundException e) {
+            throw new FileReadException("File at the path " + filePath + " not found.", e);
         } catch (IOException e) {
-            throw new RuntimeException("Couldn't read data from " + filePath, e);
+            throw new FileReadException("Couldn't read data from " + filePath, e);
         }
     }
 }
